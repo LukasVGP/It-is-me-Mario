66bf2c9b04c14e367b44e42493388369513077d1
LukasVGP <hoplite.arts@gmail.com>
2024-09-10 13:16:39 -0700

    Player
    Snail attack implementedt

--- Assets/Scripts/Enemy Scripts/SnailScript.cs
+++ Assets/Scripts/Enemy Scripts/SnailScript.cs
@@ -2,212 +2,184 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SnailScript : MonoBehaviour {
+public class SnailScript : MonoBehaviour
+{
 
-	public float moveSpeed = 1f;
-	private Rigidbody2D myBody;
-	private Animator anim;
+    public float moveSpeed = 1f;
+    private Rigidbody2D myBody;
+    private Animator anim;
+    public LayerMask playerLayer;
+    private bool moveLeft;
+    private bool canMove;
+    private bool stunned;
+    public Transform left_Collision, right_Collision, top_Collision, down_Collision;
+    private Vector3 left_Collision_Pos, right_Collision_Pos;
 
-	public LayerMask playerLayer;
+    void Awake()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        left_Collision_Pos = left_Collision.position;
+        right_Collision_Pos = right_Collision.position;
+    }
 
-	private bool moveLeft;
+    void Start()
+    {
+        moveLeft = true;
+        canMove = true;
+    }
 
-	private bool canMove;
-	private bool stunned;
+    void Update()
+    {
+        if (canMove)
+        {
+            if (moveLeft)
+            {
+                myBody.velocity = new Vector2(-moveSpeed, myBody.velocity.y);
+            }
+            else
+            {
+                myBody.velocity = new Vector2(moveSpeed, myBody.velocity.y);
+            }
+        }
+        CheckCollision();
+    }
 
-	public Transform left_Collision, right_Collision, top_Collision, down_Collision;
-	private Vector3 left_Collision_Pos, right_Collision_Pos;
+    void CheckCollision()
+    {
+        RaycastHit2D leftHit = Physics2D.Raycast(left_Collision.position, Vector2.left, 0.1f, playerLayer);
+        RaycastHit2D rightHit = Physics2D.Raycast(right_Collision.position, Vector2.right, 0.1f, playerLayer);
+        Collider2D topHit = Physics2D.OverlapCircle(top_Collision.position, 0.2f, playerLayer);
 
-	void Awake() {
-		myBody = GetComponent<Rigidbody2D> ();
-		anim = GetComponent<Animator> ();
+        if (topHit != null)
+        {
+            if (topHit.gameObject.tag == MyTags.PLAYER_TAG)
+            {
+                if (!stunned)
+                {
+                    topHit.gameObject.GetComponent<Rigidbody2D>().velocity =
+                        new Vector2(topHit.gameObject.GetComponent<Rigidbody2D>().velocity.x, 7f);
+                    canMove = false;
+                    myBody.velocity = new Vector2(0, 0);
+                    anim.Play("Stunned");
+                    stunned = true;
 
-		left_Collision_Pos = left_Collision.position;
-		right_Collision_Pos = right_Collision.position;
-	}
+                    // BEETLE CODE HERE
+                    if (tag == MyTags.BEETLE_TAG)
+                    {
+                        anim.Play("Stunned");
+                        StartCoroutine(Dead(0.5f));
+                    }
+                }
+            }
+        }
 
-	void Start () {
-		moveLeft = true;
-		canMove = true;
-	}
+        if (leftHit)
+        {
+            if (leftHit.collider.gameObject.tag == MyTags.PLAYER_TAG)
+            {
+                if (!stunned)
+                {
+                    // APPLY DAMAGE TO PLAYER
+                    leftHit.collider.gameObject.GetComponent<PlayerDamage>().DealDamage();
+                }
+                else
+                {
+                    if (tag != MyTags.BEETLE_TAG)
+                    {
+                        myBody.velocity = new Vector2(15f, myBody.velocity.y);
+                        StartCoroutine(Dead(3f));
+                    }
+                }
+            }
+        }
 
-	void Update () {
-		if (canMove) {
-			if (moveLeft) {
-				myBody.velocity = new Vector2 (-moveSpeed, myBody.velocity.y);
-			} else {
-				myBody.velocity = new Vector2 (moveSpeed, myBody.velocity.y);
-			}
-		}
+        if (rightHit)
+        {
+            if (rightHit.collider.gameObject.tag == MyTags.PLAYER_TAG)
+            {
+                if (!stunned)
+                {
+                    // APPLY DAMAGE TO PLAYER
+                    rightHit.collider.gameObject.GetComponent<PlayerDamage>().DealDamage();
+                }
+                else
+                {
+                    if (tag != MyTags.BEETLE_TAG)
+                    {
+                        myBody.velocity = new Vector2(-15f, myBody.velocity.y);
+                        StartCoroutine(Dead(3f));
+                    }
+                }
+            }
+        }
 
-		CheckCollision ();
+        // IF we don't detect collision any more do whats in {}
+        if (!Physics2D.Raycast(down_Collision.position, Vector2.down, 0.1f))
+        {
+            ChangeDirection();
+        }
+    }
 
-	}
+    void ChangeDirection()
+    {
+        moveLeft = !moveLeft;
+        Vector3 tempScale = transform.localScale;
+        if (moveLeft)
+        {
+            tempScale.x = Mathf.Abs(tempScale.x);
+            left_Collision.position = left_Collision_Pos;
+            right_Collision.position = right_Collision_Pos;
+        }
+        else
+        {
+            tempScale.x = -Mathf.Abs(tempScale.x);
+            left_Collision.position = right_Collision_Pos;
+            right_Collision.position = left_Collision_Pos;
+        }
+        transform.localScale = tempScale;
+    }
 
-	void CheckCollision() {
+    IEnumerator Dead(float timer)
+    {
+        yield return new WaitForSeconds(timer);
+        gameObject.SetActive(false);
+    }
 
-		RaycastHit2D leftHit = Physics2D.Raycast (left_Collision.position, Vector2.left, 0.1f, playerLayer);
-		RaycastHit2D rightHit = Physics2D.Raycast (right_Collision.position, Vector2.right, 0.1f, playerLayer);
+    void OnTriggerEnter2D(Collider2D target)
+    {
+        if (target.tag == MyTags.BULLET_TAG)
+        {
+            if (tag == MyTags.BEETLE_TAG)
+            {
+                anim.Play("Stunned");
+                canMove = false;
+                myBody.velocity = new Vector2(0, 0);
+                StartCoroutine(Dead(0.4f));
+            }
+            if (tag == MyTags.SNAIL_TAG)
+            {
+                if (!stunned)
+                {
+                    anim.Play("Stunned");
+                    stunned = true;
+                    canMove = false;
+                    myBody.velocity = new Vector2(0, 0);
+                }
+                else
+                {
+                    gameObject.SetActive(false);
+                }
+            }
+        }
+    }
 
-		Collider2D topHit = Physics2D.OverlapCircle (top_Collision.position, 0.2f, playerLayer);
-
-		if (topHit != null) {
-			if (topHit.gameObject.tag == MyTags.PLAYER_TAG) {
-				if (!stunned) {
-					topHit.gameObject.GetComponent<Rigidbody2D> ().velocity =
-						new Vector2 (topHit.gameObject.GetComponent<Rigidbody2D>().velocity.x, 7f);
-
-					canMove = false;
-					myBody.velocity = new Vector2 (0, 0);
-
-					anim.Play ("Stunned");
-					stunned = true;
-
-					// BEETLE CODE HERE
-					if(tag == MyTags.BEETLE_TAG) {
-						anim.Play ("Stunned");
-						StartCoroutine (Dead (0.5f));
-					}
-				}
-			}
-		}
-
-		if (leftHit) {
-			if (leftHit.collider.gameObject.tag == MyTags.PLAYER_TAG) {
-				if (!stunned) {
-					// APPLY DAMAGE TO PLAYER
-					leftHit.collider.gameObject.GetComponent<PlayerDamage>().DealDamage();
-				} else {
-					if (tag != MyTags.BEETLE_TAG) {
-						myBody.velocity = new Vector2 (15f, myBody.velocity.y);
-						StartCoroutine (Dead (3f));
-					}
-				}
-			}
-		}
-
-		if (rightHit) {
-			if (rightHit.collider.gameObject.tag == MyTags.PLAYER_TAG) {
-				if (!stunned) {
-					// APPLY DAMAGE TO PLAYER
-					rightHit.collider.gameObject.GetComponent<PlayerDamage>().DealDamage();
-				} else {
-					if (tag != MyTags.BEETLE_TAG) {
-						myBody.velocity = new Vector2 (-15f, myBody.velocity.y);
-						StartCoroutine (Dead (3f));
-					}
-				}
-			}
-		}
-
-		// IF we don't detect collision any more do whats in {}
-		if (!Physics2D.Raycast (down_Collision.position, Vector2.down, 0.1f)) {
-
-			ChangeDirection ();
-		}
-
-	}
-
-	void ChangeDirection() {
-
-		moveLeft = !moveLeft;
-
-		Vector3 tempScale = transform.localScale;
-
-		if (moveLeft) {
-			tempScale.x = Mathf.Abs (tempScale.x);
-
-			left_Collision.position = left_Collision_Pos;
-			right_Collision.position = right_Collision_Pos;
-
-		} else {
-			tempScale.x = -Mathf.Abs (tempScale.x);
-
-			left_Collision.position = right_Collision_Pos;
-			right_Collision.position = left_Collision_Pos;
-		}
-
-		transform.localScale = tempScale;
-
-	}
-
-	IEnumerator Dead(float timer) {
-		yield return new WaitForSeconds (timer);
-		gameObject.SetActive (false);
-	}
-
-	void OnTriggerEnter2D(Collider2D target) {
-		if (target.tag == MyTags.BULLET_TAG) {
-
-			if (tag == MyTags.BEETLE_TAG) {
-				anim.Play ("Stunned");
-
-				canMove = false;
-				myBody.velocity = new Vector2 (0, 0);
-
-				StartCoroutine (Dead (0.4f));
-			}
-
-			if (tag == MyTags.SNAIL_TAG) {
-				if (!stunned) {
-					
-					anim.Play ("Stunned");
-					stunned = true;
-					canMove = false;
-					myBody.velocity = new Vector2 (0, 0);
-
-				} else {
-					gameObject.SetActive (false);
-				}
-			}
-
-		}
-	}
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == MyTags.PLAYER_TAG && !stunned)
+        {
+            collision.gameObject.GetComponent<PlayerDamage>().DealDamage();
+        }
+    }
 
 } // class
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- Assets/Scripts/Player Scripts/PlayerDamage.cs
+++ Assets/Scripts/Player Scripts/PlayerDamage.cs
@@ -8,6 +8,11 @@ public class PlayerDamage : MonoBehaviour
     public int lifeScoreCount;
     private bool canDamage;
     public Transform respawnPoint;
+    private Rigidbody2D rb;
+    private Animator anim;
+    public float repelForce = 5f;
+    public float deathJumpForce = 10f;
+    private bool isDead = false;
 
     void Awake()
     {
@@ -15,6 +20,8 @@ public class PlayerDamage : MonoBehaviour
         lifeScoreCount = 3;
         UpdateLifeText();
         canDamage = true;
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     void Start()
@@ -24,11 +31,22 @@ public class PlayerDamage : MonoBehaviour
 
     public void DealDamage()
     {
-        if (canDamage)
+        if (canDamage && !isDead)
         {
             lifeScoreCount--;
             UpdateLifeText();
-            CheckGameOver();
+
+            if (lifeScoreCount > 0)
+            {
+                // First hit: repel
+                RepelPlayer();
+            }
+            else
+            {
+                // Second hit: death
+                StartCoroutine(DeathSequence());
+            }
+
             canDamage = false;
             StartCoroutine(WaitForDamage());
         }
@@ -36,14 +54,46 @@ public class PlayerDamage : MonoBehaviour
 
     public void TakeDamage(int damage)
     {
-        if (canDamage)
+        if (canDamage && !isDead)
         {
-            lifeScoreCount -= damage / 100;
+            lifeScoreCount -= damage / 50; // Adjust this value to balance boss damage
             UpdateLifeText();
-            CheckGameOver();
+
+            if (lifeScoreCount > 0)
+            {
+                RepelPlayer();
+            }
+            else
+            {
+                StartCoroutine(DeathSequence());
+            }
+
             canDamage = false;
             StartCoroutine(WaitForDamage());
         }
+    }
+
+    private void RepelPlayer()
+    {
+        Vector2 repelDirection = transform.right * (transform.localScale.x > 0 ? -1 : 1);
+        rb.velocity = Vector2.zero;
+        rb.AddForce(repelDirection * repelForce, ForceMode2D.Impulse);
+    }
+
+    private IEnumerator DeathSequence()
+    {
+        isDead = true;
+        anim.SetTrigger("Death");
+        rb.velocity = Vector2.zero;
+        rb.AddForce(Vector2.up * deathJumpForce, ForceMode2D.Impulse);
+
+        yield return new WaitForSeconds(1f);
+
+        gameObject.SetActive(false);
+
+        yield return new WaitForSeconds(1f);
+
+        Respawn();
     }
 
     private void UpdateLifeText()
@@ -51,24 +101,14 @@ public class PlayerDamage : MonoBehaviour
         lifeText.text = "x" + Mathf.Max(lifeScoreCount, 0);
     }
 
-    private void CheckGameOver()
-    {
-        if (lifeScoreCount <= 0)
-        {
-            if (GameManager.Instance != null)
-            {
-                GameManager.Instance.GameOver();
-            }
-            else
-            {
-                Debug.LogError("GameManager instance not found!");
-            }
-        }
-    }
-
     public void Respawn()
     {
+        isDead = false;
         transform.position = respawnPoint.position;
+        gameObject.SetActive(true);
+        lifeScoreCount = 3;
+        UpdateLifeText();
+        anim.SetTrigger("Respawn");
     }
 
     IEnumerator WaitForDamage()
