b071b22ddaceae92cf3595aa4426a8a9779d1140
LukasVGP <hoplite.arts@gmail.com>
2024-09-17 09:36:52 -0700

    fix score scribts

--- Assets/Scripts/Player Scripts/ScoreScript.cs
+++ Assets/Scripts/Player Scripts/ScoreScript.cs
@@ -1,8 +1,11 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class ScoreScript : MonoBehaviour
 {
+
     private Text coinTextScore;
     private AudioSource audioManager;
     private int scoreCount;
@@ -15,25 +18,64 @@ public class ScoreScript : MonoBehaviour
     void Start()
     {
         coinTextScore = GameObject.Find("CoinText").GetComponent<Text>();
-        scoreCount = PlayerPrefs.GetInt("Score", 0);
-        UpdateScoreText();
     }
 
     void OnTriggerEnter2D(Collider2D target)
     {
         if (target.tag == MyTags.COIN_TAG)
         {
+
             target.gameObject.SetActive(false);
             scoreCount++;
-            UpdateScoreText();
-            PlayerPrefs.SetInt("Score", scoreCount);
-            PlayerPrefs.Save();
+
+            coinTextScore.text = "x" + scoreCount;
+
             audioManager.Play();
         }
     }
 
-    void UpdateScoreText()
-    {
-        coinTextScore.text = "x" + scoreCount;
-    }
-}
+} // class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
